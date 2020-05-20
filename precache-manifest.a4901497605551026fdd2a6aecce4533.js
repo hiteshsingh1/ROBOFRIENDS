b663c26de122513aa3d1080ceddd9aa52d92b451
hiteshsingh1 <42681784+hiteshsingh1@users.noreply.github.com>
2020-05-20 19:49:45 +0530

    Updates

---
+++ precache-manifest.a4901497605551026fdd2a6aecce4533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8902f70b28c9657db5bebab13e9c8d3c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "154b1a806f56525539b4",
+    "url": "/robofriends/static/css/main.b20bb78a.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154b1a806f56525539b4",
+    "url": "/robofriends/static/js/main.f12d21dc.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
